501f2feb2d7d7a233aaa260c95aa8f0f86fd035c
lithium <code@hlidskialf.com>
2016-02-11 19:21:48 -0800

    upgrade to django 1.9

--- apps/mainsite/urls.py
+++ apps/mainsite/urls.py
@@ -7,8 +7,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-from mainsite.views import HomePageView
-
+from mainsite.views import HomePageView, error404, error500
 
 handler404 = 'mainsite.views.error404'
 handler500 = 'mainsite.views.error500'
@@ -33,27 +32,30 @@ urlpatterns = [
 #
 ###
 
+
 # Test URLs to allow you to see these pages while DEBUG is True
 if getattr(settings, 'DEBUG_ERRORS', False):
-    urlpatterns = patterns('mainsite.views',
-        url(r'^error/404$', 'error404', name='404'),
-        url(r'^error/500$', 'error500', name='500'),
-    ) + urlpatterns
+    urlpatterns = [
+        url(r'^error/404$', error404, name='error_404'),
+        url(r'^error/500$', error500, name='error_500'),
+    ] + urlpatterns
 
 # If DEBUG_MEDIA is set, have django serve anything in MEDIA_ROOT at MEDIA_URL
 if getattr(settings, 'DEBUG_MEDIA', True):
+    from django.views.static import serve as static_serve
     media_url = getattr(settings, 'MEDIA_URL', '/media/').lstrip('/')
-    urlpatterns = patterns('',
-        url(r'^%s(?P<path>.*)$' % (media_url,), 'django.views.static.serve', {
+    urlpatterns = [
+        url(r'^%s(?P<path>.*)$' % (media_url,), static_serve, {
             'document_root': settings.MEDIA_ROOT
         }),
-    ) + urlpatterns
+    ] + urlpatterns
 
 # If DEBUG_STATIC is set, have django serve up static files even if DEBUG=False
 if getattr(settings, 'DEBUG_STATIC', True):
     static_url = getattr(settings, 'STATIC_URL', '/static/').lstrip('/')
-    urlpatterns = patterns('',
-        url(r'^%s(?P<path>.*)' % (static_url,), 'django.contrib.staticfiles.views.serve', kwargs={
+    from django.contrib.staticfiles.views import serve as staticfiles_serve
+    urlpatterns = [
+        url(r'^%s(?P<path>.*)' % (static_url,), staticfiles_serve, kwargs={
             'insecure': True,
         })
-    ) + urlpatterns
+    ] + urlpatterns
